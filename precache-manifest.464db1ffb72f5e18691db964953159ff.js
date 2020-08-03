fdef6c60084ec06b784f6abdb5344198e3f10cdb
Reeti Sharma <reetisharma105@gmail.com>
2020-08-03 20:31:47 +0530

    Updates

---
+++ precache-manifest.464db1ffb72f5e18691db964953159ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557354a98de2c4c282096ee213b316c0",
+    "url": "/GithubFinder/index.html"
+  },
+  {
+    "revision": "b77a6f42ac27037cd5c4",
+    "url": "/GithubFinder/static/css/main.5ebc5e4e.chunk.css"
+  },
+  {
+    "revision": "1547b6a73e6cc93d7ec7",
+    "url": "/GithubFinder/static/js/2.83b09fb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GithubFinder/static/js/2.83b09fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b77a6f42ac27037cd5c4",
+    "url": "/GithubFinder/static/js/main.74098c90.chunk.js"
+  },
+  {
+    "revision": "e492ba030311e65a0e65",
+    "url": "/GithubFinder/static/js/runtime-main.312db043.js"
+  }
+]);
