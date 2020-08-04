0582fd2e1172234ee34313ebabc9fd7453886603
Reeti Sharma <63422081+reeticodes@users.noreply.github.com>
2020-08-04 09:01:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,11 +22,11 @@ const App = ()=> {
         <div className="container">
         <Alert />
         <Switch>
-          <Route exact path ='/' component={Home}
+          <Route exact path ='/GithubFinder' component={Home}
           />
           <Route exact path='/about' component={About}  />
           <Route exact path= '/user/:login'component={User} />
-          <Route component={NotFound} />
+          <Route  exact path ='*' component={NotFound} />
         </Switch>
           
         </div>
